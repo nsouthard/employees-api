7f6a47e6bde19b04fd08a7f512e4cce7b051924c
nsouthard <nathan.southard@gmail.com>
2017-06-06 21:34:58 -0500

    end of night save

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def create
+    employee = Employee.create(
+                                first_name: params[:first_name],
+                                last_name: params[:last_name],
+                                email: params[:email]
+                                )
+    render :show
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(
+                      first_name: params[:first_name],
+                      last_name: params[:last_name],
+                      email: params[:email]
+                      )
+    render :show
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redner json: {message: "Employee Destroyed"}
+  end
+
+end
---
+++ app/views/api/v2/employees/_employee.json.jbuilder
@@ -0,0 +1,6 @@
+
+  json.id employee.id
+  json.full_name employee.full_name 
+  json.email employee.email
+  json.birthday employee.birthdate
+
